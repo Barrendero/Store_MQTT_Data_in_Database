f2051e704fb46d483fb42fd653880e260c1b8757
Joel_Barrendero <83477393+Barrendero@users.noreply.github.com>
2022-02-14 20:53:02 -0600

    Update mqtt_Publish_Dummy_Data.py
    This was a little test using Fake data, simulating we are sending data from an ESP32 module oxygen variable.

--- mqtt_Publish_Dummy_Data.py
+++ mqtt_Publish_Dummy_Data.py
@@ -1,6 +1,6 @@
 #------------------------------------------
-#--- Author: Pradeep Singh
-#--- Date: 20th January 2017
+#--- Author: Barrendero
+#--- Date: 14th February 2022
 #--- Version: 1.0
 #--- Python Ver: 2.7
 #--- Details At: https://iotbytes.wordpress.com/store-mqtt-data-from-sensors-into-sql-database/
@@ -13,20 +13,21 @@ from datetime import datetime
 
 #====================================================
 # MQTT Settings 
-MQTT_Broker = "iot.eclipse.org"
+MQTT_Broker = "localhost"
 MQTT_Port = 1883
 Keep_Alive_Interval = 45
 MQTT_Topic_Humidity = "Home/BedRoom/DHT22/Humidity"
 MQTT_Topic_Temperature = "Home/BedRoom/DHT22/Temperature"
+MQTT_Topic_Oxygen = "Home/BedRoom/ud/Oxygen"
 
 #====================================================
 
 def on_connect(client, userdata, rc):
 	if rc != 0:
 		pass
-		print "Unable to connect to MQTT Broker..."
+		print ("Unable to connect to MQTT Broker...")
 	else:
-		print "Connected with MQTT Broker: " + str(MQTT_Broker)
+		print ("Connected with MQTT Broker: " + str(MQTT_Broker))
 
 def on_publish(client, userdata, mid):
 	pass
@@ -45,7 +46,7 @@ mqttc.connect(MQTT_Broker, int(MQTT_Port), int(Keep_Alive_Interval))
 def publish_To_Topic(topic, message):
 	mqttc.publish(topic,message)
 	print ("Published: " + str(message) + " " + "on MQTT Topic: " + str(topic))
-	print ""
+	print ("")
 
 
 #====================================================
@@ -67,11 +68,11 @@ def publish_Fake_Sensor_Values_to_MQTT():
 		Humidity_Data['Humidity'] = Humidity_Fake_Value
 		humidity_json_data = json.dumps(Humidity_Data)
 
-		print "Publishing fake Humidity Value: " + str(Humidity_Fake_Value) + "..."
+		print ("Publishing fake Humidity Value: " + str(Humidity_Fake_Value) + "...")
 		publish_To_Topic (MQTT_Topic_Humidity, humidity_json_data)
 		toggle = 1
 
-	else:
+	if toggle == 1:
 		Temperature_Fake_Value = float("{0:.2f}".format(random.uniform(1, 30)))
 
 		Temperature_Data = {}
@@ -80,11 +81,25 @@ def publish_Fake_Sensor_Values_to_MQTT():
 		Temperature_Data['Temperature'] = Temperature_Fake_Value
 		temperature_json_data = json.dumps(Temperature_Data)
 
-		print "Publishing fake Temperature Value: " + str(Temperature_Fake_Value) + "..."
+		print ("Publishing fake Temperature Value: " + str(Temperature_Fake_Value) + "...")
 		publish_To_Topic (MQTT_Topic_Temperature, temperature_json_data)
+		toggle = 2
+
+	if toggle == 2:
+		Oxygen_Fake_Value = float("{0:.2f}".format(random.uniform(10, 20)))
+
+		Oxygen_Data = {}
+		#Udself_Data['Sensor_ID'] = "Dummy-3"
+		#Udself_Data['Date'] = (datetime.today()).strftime("%d-%b-%Y %H:%M:%S:%f")
+		Oxygen_Data['Oxygen'] = Oxygen_Fake_Value
+		Oxygen_json_data = json.dumps(Oxygen_Data)
+
+		print ("Publishing fake Oxygen Value: " + str(Oxygen_Fake_Value) + "...")
+		publish_To_Topic (MQTT_Topic_Oxygen, Oxygen_json_data)
 		toggle = 0
 
 
 publish_Fake_Sensor_Values_to_MQTT()
 
 #====================================================
+
